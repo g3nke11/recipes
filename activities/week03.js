f66b0b2667712e471b52c9d462e5e3dfe58a295a
g3nke11 <gisaack@byui.edu>
2024-05-06 11:05:11 -0600

    updates

--- activities/week03.js
+++ activities/week03.js
@@ -1,2 +1,9 @@
-let ourImage = document.getElementById('testImage')
-ourImage.src = "../mission/images/byui-logo_blue.webp"
+let imageInput = document.getElementById('imageInput');
+let imageButton = document.getElementById('imageButton');
+let ourImage = document.getElementById('testImage');
+
+imageButton.addEventListener('click', function() {
+    let newImageURL = imageInput.value;
+    // ourImage.src = "../mission/images/byui-logo_blue.webp"
+    ourImage.src = newImageURL;
+})
