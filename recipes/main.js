965c0557b432924c2de78afb45d9d64e951db92b
g3nke11 <gisaack@byui.edu>
2024-06-22 13:59:50 -0600

    performance updates

--- recipes/main.js
+++ recipes/main.js
@@ -1,14 +1,14 @@
-import { recipes } from "./recipes.mjs";
+// import { recipes } from "./recipes.mjs";
 
-function addArticles() {
-    var articleGrid = document.querySelector('.article-grid');
-    recipes.forEach(article => {
-      var htmltoinsert = articleTemplate(recipes.author, recipes.url, recipes.isBasedOn, recipes.cookTime, recipes.datePublished, recipes.tags, recipes.description, recipes.image, recipes.recipeIngredient, recipes.name, recipes.prepTime, recipes.recipeInstructions, recipes.recipeYield, recipes.rating);
-      var articleElement = document.createElement('div');
-      // articleElement.innerHTML = htmltoinsert;
-      // articleGrid.appendChild(articleElement);
-      articleGrid.insertAdjacentHTML('beforeend', htmltoinsert)
-    });
-  };
+// function addArticles() {
+//     var articleGrid = document.querySelector('.article-grid');
+//     recipes.forEach(article => {
+//       var htmltoinsert = articleTemplate(recipes.author, recipes.url, recipes.isBasedOn, recipes.cookTime, recipes.datePublished, recipes.tags, recipes.description, recipes.image, recipes.recipeIngredient, recipes.name, recipes.prepTime, recipes.recipeInstructions, recipes.recipeYield, recipes.rating);
+//       var articleElement = document.createElement('div');
+//       // articleElement.innerHTML = htmltoinsert;
+//       // articleGrid.appendChild(articleElement);
+//       articleGrid.insertAdjacentHTML('beforeend', htmltoinsert)
+//     });
+//   };
   
 //   document.addEventListener('DOMContentLoaded', addArticles)
