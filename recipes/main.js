6dfa3c48aa7074ed02d96ab9b2965446a11dde0f
g3nke11 <gisaack@byui.edu>
2024-06-26 17:00:58 -0600

    adding search function

--- recipes/main.js
+++ recipes/main.js
@@ -1,14 +1,102 @@
-// import { recipes } from "./recipes.mjs";
+import recipes from "./recipes.mjs";
 
-// function addArticles() {
-//     var articleGrid = document.querySelector('.article-grid');
-//     recipes.forEach(article => {
-//       var htmltoinsert = articleTemplate(recipes.author, recipes.url, recipes.isBasedOn, recipes.cookTime, recipes.datePublished, recipes.tags, recipes.description, recipes.image, recipes.recipeIngredient, recipes.name, recipes.prepTime, recipes.recipeInstructions, recipes.recipeYield, recipes.rating);
-//       var articleElement = document.createElement('div');
-//       // articleElement.innerHTML = htmltoinsert;
-//       // articleGrid.appendChild(articleElement);
-//       articleGrid.insertAdjacentHTML('beforeend', htmltoinsert)
-//     });
-//   };
-  
-//   document.addEventListener('DOMContentLoaded', addArticles)
+function random(num) {
+    return Math.floor(Math.random() * num);
+}
+
+function getRandomListEntry(list) {
+    const listLength = list.length;
+    const randomNum = random(listLength);
+    return list[randomNum]
+}
+
+function tagsTemplate(tags) {
+    let html = ``
+    tags.forEach(tag => {
+        html += `<li>${tag}</li>`
+    });
+    console.log(html)
+    return html;
+}
+
+function ratingTemplate(rating) {
+    let html = `<span
+    class="rating"
+    role="img"
+    aria-label="Rating: ${rating} out of 5 starts
+    >`
+    for (let i = 1; i <= 5; i++) {
+        if (i <= rating) {
+            html += `<span aria-hidden="true" class="icon-star">⭐</span>`
+        }
+        else {
+            html += `<span aria-hidden="true" class="icon-star-empty">☆</span>`
+        }
+    }
+
+    html += `</span>`
+    return html
+}
+
+function recipeTemplate(recipe) {
+	return `
+    <figure class="recipe">
+	    <img class="recipe-img" src=${recipe.image} alt=${recipe.name} />
+	    <figcaption>
+	    	<ul class="recipe__tags">
+	    		${tagsTemplate(recipe.tags)}
+	    	</ul>
+	    	<h2><a href="#">${recipe.name}</a></h2>
+	    	<p class="recipe__ratings">
+                ${ratingTemplate(recipe.rating)}			
+	    	</p>
+	    	<p class="recipe__description">
+	    		${recipe.description}
+	    	</p>
+        </figcaption>
+    </figure>
+    `;
+}
+
+function renderRecipes(recipeList) {
+    const recipeContainer = document.querySelector('.recipe-container');
+    recipeContainer.innerHTML = '';
+
+    recipeList.forEach(recipe => {
+        const recipeHTML = recipeTemplate(recipe);
+        console.log("renderRecipes: ",recipe)
+        recipeContainer.insertAdjacentHTML("beforeend", recipeHTML);
+    })
+}
+
+function init() {
+    const recipe = getRandomListEntry(recipes);
+    console.log(recipe);
+    renderRecipes([recipe]);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    init();
+});
+
+function filter(query) {
+    const filtered = recipes.filter(recipe => {
+        return recipe.name.toLowerCase().includes(query) ||
+                recipe.description.toLowerCase().includes(query) ||
+                recipe.tags.some(tag => tag.toLowerCase().includes(query));
+    });
+    console.log(filtered);
+    const sorted = filtered.sort((a, b) => a.name.localeCompare(b.name));
+    console.log(sorted);
+    return sorted;
+}
+
+function searchHandler(e) {
+    // e.preventDafult();
+    const searchTerm = e.target.value.toLowerCase();
+    const filterRecipes = filter(searchTerm);
+    renderRecipes(filterRecipes);
+    console.log("handler done");
+}
+
+document.querySelector('#search-bar').addEventListener('search', searchHandler);
