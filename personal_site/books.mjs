c7f4cc6b569dabf41a9119f5cc5e78ab8838ab6f
g3nke11 <gisaack@byui.edu>
2024-07-10 17:23:49 -0600

    fixing book array

--- personal_site/books.mjs
+++ personal_site/books.mjs
@@ -4,22 +4,36 @@ const books = [
 		tags: ['Fiction'],
 		description: '<p><strong>Pawn of Prophecy:</strong> The Pawn of Prophecy is a fantasy book written by David Eddings and published by Grim Oak Press. It is the first book in the Belgariad series, which contains a total of five main books with additional companion volumes.<br> Book Description from publisher: Long ago, so the Storyteller claimed, the evil god Torak sought dominion and drove men and gods to war. But Belgrath the Sorcerer led men to reclaim the Orb that protected the West. So long as it lay at Riva, the prophecy went, men would be safe. That was only a  story, and Garion did not believe in magic dooms, even though the man without a shadow had haunted him for years. Brought up on a quiet farm by his Aunt Pol, how could he know that the Apostate planned to wake dread Torak, or that he would be led on a quest of unparalleled magic and danger by those he loved, but did not truly know.<br> For yet a little while, his dreams of innocence remained safe, untroubled by knowledge of his strange heritage. For a little while... thus begins The Belgariad, an epic fantasy of immense scope set against a history of 7,000 years. It tells of the struggles between ancient gods and mighty Kings, and of men in strange lands facing fated events, all bound by a prophecy that must be fulfilled.<br><br></p>',
 		cover: './images/Book_Photos/rsz_belgariad_pawnofprophecy_grimoakpress.jpg',
-		title: 'Pawn of Prophecy',
+		title: 'Pawn of Prophecy'
 	},
     {
 		author: 'Provo High Culinary Students',
 		tags: ['Fiction'],
 		description: '<p><strong>Fablehaven:</strong> Fablehaven is a fantasy book written by Brandon Mull and published by Simon and Schuster. Fablehaven is the first book in the five volume Fablehaven series.<br> Book Description from publisher: For centuries mystical creatures of all description were gathered into a hidden refuge called Fablehaven to prevent their extinction. The sanctuary survives today as one of the last strongholds of true magic. Enchanting? Absolutely. Exciting? You bet. Safe? Well, actually, quite the opposite.<br> Kendra and her brother, Seth, have no idea that their grandfather is the current caretaker of Fablehaven. Inside the gated woods, ancient laws keep relative order among greedy trolls, mischievous satyrs, plotting witches, spiteful imps, and jealous fairies. However, when the rules get broken—Seth is a bit too curious and reckless for his own good—powerful forces of evil are unleashed, and Kendra and her brother face the greatest challenge of their lives. To save their family, Fablehaven, and perhaps even the world, Kendra and Seth must find the courage to do what they fear most.</p>',
 		cover: './images/Book_Photos/rsz_fablehaven_simonandschuster.jpg',
-		title: 'Fablehaven',
+		title: 'Fablehaven'
 	},
     {
 		author: 'Provo High Culinary Students',
 		tags: ['Fiction'],
 		description: '<p><strong>The Fellowship of the Ring:</strong> The Fellowship of the Ring was written by J.R.R. Tolkien and originally published by Allen & Unwin Book Publishers in 1954. This book is still a classic read along with the following two books in this three book series.<br> Book Description by publisher: One Ring to rule them all, One Ring to find them, One Ring to bring them all and in darkness bind them.<br> In ancient times the Rings of Power were crafted by the Elven-smiths, and Sauron, the Dark Lord, forged the One Ring, filling it with his own power so that he could rule all others. But the One Ring was taken from him, and though he sought it throughout Middle-earth, it remained lost to him. After many ages it fell into the hands of Bilbo Baggins, as told in The Hobbit. In a sleepy village in the Shire, young Frodo Baggins finds himself faced with an immense task, as his elderly cousin Bilbo entrusts the Ring to his care. Frodo must leave his home and make a perilous journey across Middle-earth to the Cracks of Doom, there to destroy the Ring and foil the Dark Lord in his evil purpose.<br><br></p>',
 		cover: './images/Book_Photos/rsz_thefellowshipofthering_harpercollins.jpg',
-		title: 'The Fellowship of the Ring',
-	}];
+		title: 'The Fellowship of the Ring'
+	},
+    {
+        author: 'Provo High Culinary Students',
+		tags: ['Non-Fiction'],
+		description: "<p>The 7 Habits of Highly Effective People: The 7 Habits of Highly Effective People was written by Stephen R. Covey and is published by Simon and Schuster. Book Description by publisher: The 7 Habits have become famous and are integrated into everyday thinking by millions and millions of people. Why? Because they work! With Sean Covey's added takeaways on how the habits can be used in our modern age, the wisdom of the 7 Habits will be refreshed for a new generation of leaders.<br><br> They include:</p> <ul><li>Habit 1: Be Proactive</li><li>Habit 2: Begin with the End in Mind</li><li>Habit 3: Put First Things First</li><li>Habit 4: Think Win/Win</li><li>Habit 5: Seek First to Understand, Then to Be Understood</li><li>Habit 6: Synergize</li><li>Habit 7: Sharpen the Saw</li></ul>",
+		cover: './images/Book_Photos/rsz_the7habitsofhighlyeffectivepeople_simonandschuster.jpg',
+		title: '7 Habits of Highly Effective People'
+    },
+    {
+        author: 'Provo High Culinary Students',
+		tags: ['Non-Fiction'],
+		description: "<p>The Book of Mormon: The Book of Mormon is an ancient record of the original inhabitant of the Americas published by the Church of Jesus Christ of Latter-Day Saints. It spans over a thousand years of history that includes both personal accounts and the events of entire peoples.<br> The primary purpose of the Book of Mormon is the testify of Jesus Christ. It is to serve as a companion volume to the Bible and serves to clarify the teachings of ancient prophets. The book was written by many ancient prophets by the spirit of prophecy and revelation. Their words, written on gold plates, were quoted and abridged by a prophet-historian named Mormon. The record gives an account of two great civilizations. One came from Jerusalem in 600 B.C. and afterward separated into two nations, known as the Nephites and the Lamanites. The other came much earlier when the Lord confounded the tongues at the Tower of Babel. This group is known as the Jaredites. After thousands of years, all were destroyed except the Lamanites, and they are among the ancestors of the American Indians.</p>",
+		cover: './images/TheBookOfMormon_LDS.jpg',
+		title: 'The Book of Mormon'
+    }];
     
 
 export default books
